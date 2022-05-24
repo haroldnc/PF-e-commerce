d8773082212456fc3a25d3764df23bbe2ccc8f92
Alejandro Cardenas <ogopogo@fibertel.com.ar>
2022-05-24 14:13:20 -0300

    Validaciones tabla User

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -3,34 +3,61 @@ const { Schema, model } = require('mongoose');
 const User = Schema({
     username: {
         type: String,
-        require: true
+        require: [true, 'Username is required'],
     },
     firstName: {
         type: String,
-        require: true
+        require: [true, 'First name is required'],
+        validate: {
+            validator: (v) => /^[a-z]+$/i.test(v),
+            message: props => `First name only accept letters`
+         }
     },
     lastName: {
         type: String,
-        require: true
+        require: [true, 'Last name is required'],
+        validate: {
+            validator: (v) => /^[a-z]+$/i.test(v),
+            message: props => `Last name only accept letters`
+         }
     },
     email: {
         type: String,
-        require: true
+        require: [true, 'User mail is required'],
+        validate: {
+            validator: (v) => /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/i.test(v),
+            message: props => `the email is not valid`
+        }
     },
+        /*  Password valida Minimo 8 caracteres 
+        Maximo 15
+        Al menos una letra mayuscula
+        Al menos una letra minuscula
+        Al menos un digito
+        No espacios en blanco
+        Al menos 1 caracter especial*/
     password: {
         type: String,
-        require: true
+        require: [true, 'Password is required'],
+        validate: {
+            validator: (v) => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/i.test(v),
+            message: props => `Password is not valid`
+            }
     },
     image: {
         type: String
     },
-    user_type: {
+    user_role: {
             type: Schema.Types.ObjectId,
-            ref: 'User_roles'  
+            ref: 'roles'  
+    },
+    dni: {
+        type: Number,
+        require: [true, 'DNI number is required'],
     },
     phone: {
         type: String,
-        require: true
+        require: [true, 'Phone number is required'],
     },
     web: {
         type: String
@@ -38,14 +65,21 @@ const User = Schema({
     linkedin: {
         type: String 
     },
-    // estado del usuario confirmacion de mail
+    publications: {
+        type: [Schema.Types.ObjectId],
+        ref: ''
+    },
+    punctuation: {
+        type: Number
+    },
+    // estado del usuario confirmacion de mail? Evaluar si queda o no.
     confirm_email: {
         type: Boolean,
         default: false
     }
-    
 });
-//? schema methods for user model
+
+// schema methods for user model
 UserSchema.method('toJSON', function () {
     const { __v, _id, password, ...object } = this.toObject();
     object.uid = _id;
