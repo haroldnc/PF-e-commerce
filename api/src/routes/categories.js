815ebabaae62a9adb5e3cbfcbcd94835a0fcd588
haroldnc <harold.mth95@gmail.com>
2022-05-25 18:36:45 -0500

    Rutas, put listas para pruebas

--- api/src/routes/categories.js
+++ api/src/routes/categories.js
@@ -2,7 +2,9 @@ const { Router } = require('express');
 const {
    getCategories,
    getCategoryById,
-   addCategory
+   addCategory,
+   updateCategory,
+   updateService
 } = require('../controllers/categories.controller');
 
 const router = Router();
@@ -10,5 +12,7 @@ const router = Router();
 router.get('/', getCategories);
 router.get('/:idCategory', getCategoryById);
 router.post('/', addCategory);
+router.put('/', updateCategory);
+router.put('/:idCategory', updateService);
 
 module.exports = router;
