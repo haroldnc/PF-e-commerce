eb80f90c4a18a07ebfc1b151e393436c276a1c0b
MateoM1608 <madime-1997@hotmail.com>
2022-05-25 18:00:04 -0500

    Agrego componente LogosHome a Home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Carousel from '../../components/Carousel/Carousel'
 import Hero from '../../components/Hero/Hero'
 import Presentation from '../../components/Presentation/Presentation'
 import Testimonials from '../../components/Testimonials/Testimonials'
+import LogosHome from '../../components/LogosHome/LogosHome'
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Hero />
       <Carousel />
       <Presentation />
+      <LogosHome/>
       <Testimonials/>
     </>
   )
