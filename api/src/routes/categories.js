6448f0a7d07f5416c593cfaab8691525f22036ea
Alejandro Cardenas <ogopogo@fibertel.com.ar>
2022-05-26 13:31:07 -0300

    Creo validacion para user admin

--- api/src/routes/categories.js
+++ api/src/routes/categories.js
@@ -13,10 +13,10 @@ const { validarADMIN_ROLE } = require('../middlewares/validarAdminRole');
 const router = Router();
 
 router.get('/', getCategories);
-router.post('/', addCategory);
-router.put('/', updateCategory);
+router.post('/', validarADMIN_ROLE, addCategory);
+router.put('/', validarADMIN_ROLE, updateCategory);
 router.get('/:idCategory', getCategoryById);
-router.put('/:idCategory', updateService);
-router.delete('/:idCategory', deleteCategory);
+router.put('/:idCategory', validarADMIN_ROLE, updateService);
+router.delete('/:idCategory', validarADMIN_ROLE, deleteCategory);
 
 module.exports = router;
