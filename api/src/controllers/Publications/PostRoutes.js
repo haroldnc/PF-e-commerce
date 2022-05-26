9850b7c2ad146e66d3da69b17a8e18ce640bdd61
damian sola <damiansola99@gmail.com>
2022-05-25 22:12:08 -0300

    cree la ruta put y delete de las publicaciones

---
+++ api/src/controllers/Publications/PostRoutes.js
@@ -0,0 +1,38 @@
+const Publications =require("../../models/Publications")
+
+const upDatePost = async (req, res, next) => {
+    let { id } = req.params;
+    let post = req.body;
+
+    try{
+        const newPost = {
+            title: post.title,
+            description: post.description,
+        }
+    
+        let result = await Publications.findByIdAndUpdate(id, newPost)
+        res.send(result)
+    }catch(error){
+        console.log(error)
+        next(error)
+    }  
+}
+
+
+const deletePost = async (req, res, next) => {
+    let {id} = req.params;
+
+    try{
+        await Publications.findByIdAndDelete(id)
+        res.send({msg:"the post was deleted"})
+    }catch(error){
+        console.log(error)
+        next(error)
+    }
+}
+
+
+module.exports={
+    upDatePost,
+    deletePost
+}
