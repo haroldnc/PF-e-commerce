dec79c8d942284eb87d77ca823812402a4c0ee83
MateoM1608 <madime-1997@hotmail.com>
2022-05-29 00:38:22 -0500

    Cuadrando logos

--- client/src/components/LogosHome/LogosHome.jsx
+++ client/src/components/LogosHome/LogosHome.jsx
@@ -2,74 +2,53 @@ import React from "react";
 import { LogosGlobal, LogoCard, Logo, NameCard, Name} from './StyledLogosHome'
 import { useHistory } from "react-router-dom";
 
-import Data from '../assets/LogosHome/Data.JPG'
-import DiseñoGrafico from '../assets/LogosHome/Diseño.JPG'
-import EstiloDeVida from '../assets/LogosHome/Estilo.JPG'
-import MarketingDigital from '../assets/LogosHome/Marketing.JPG'
-import MusicaAudio from '../assets/LogosHome/Musica.JPG'
-import Negocios from '../assets/LogosHome/Negocios.JPG'
-import Programacion from '../assets/LogosHome/Programacion.JPG'
-import Traduccion from '../assets/LogosHome/Traduccion.JPG'
-import Video from '../assets/LogosHome/Video o animacion.JPG'
+import {IconContext} from 'react-icons'
+import { GiCoffeeCup } from 'react-icons/gi' //Estilo de vida
+import { RiComputerLine } from 'react-icons/ri' // Marketing Digital
+import { MdOndemandVideo, MdOutlineLibraryMusic, MdDeveloperMode } from 'react-icons/md' // Video y Animacion, musica y audio, desarrollo
+import { GiBookmarklet, GiShakingHands } from 'react-icons/gi' // traduccion, negocios
+import { BsClipboardData, BsPencilSquare } from 'react-icons/bs' // Data , diseño grafico
 
 
+
+// import Data from '../assets/LogosHome/Data.JPG'
+// import DiseñoGrafico from '../assets/LogosHome/Diseño.JPG'
+// import EstiloDeVida from '../assets/LogosHome/Estilo.JPG'
+// import MarketingDigital from '../assets/LogosHome/Marketing.JPG'
+// import MusicaAudio from '../assets/LogosHome/Musica.JPG'
+// import Negocios from '../assets/LogosHome/Negocios.JPG'
+// import Programacion from '../assets/LogosHome/Programacion.JPG'
+// import Traduccion from '../assets/LogosHome/Traduccion.JPG'
+// import Video from '../assets/LogosHome/Video o animacion.JPG'
 
 
 const LogosHome = ({allCategories}) => {
 
     const history = useHistory()
-    
-    const marketplace = [
-        {name:"Diseñadores Graficos",
-         logo: DiseñoGrafico,
-         id: 1
-        },{
-            name: "Marketing Digital",
-            logo: MarketingDigital,
-            id: 2
-        },{
-            name: "Escritura y Traducción",
-            logo: Traduccion,
-            id: 3
-        },{
-            name: "Video y Animación",
-            logo: Video,
-            id: 4
-        },{
-            name: "Musica y Audio",
-            logo: MusicaAudio,
-            id: 5
-        },{
-            name: "Programación y Tecnologia",
-            logo: Programacion,
-            id: 6
-        },{
-            name: "Negocios",
-            logo: Negocios,
-            id: 7
-        },{
-            name: "Estilo de vida",
-            logo: EstiloDeVida,
-            id: 8
-        },{
-            name: "Data",
-            logo: Data,
-            id: 9
-        }
+    const allLogos =[
+    <BsPencilSquare size={32} style={{color: 'rgb(138, 135, 135)'}} />, 
+    <RiComputerLine size={32} style={{color: 'rgb(138, 135, 135)'}} />, 
+    <GiBookmarklet size={32} style={{color: 'rgb(138, 135, 135)'}} />,
+    <MdOndemandVideo size={32} style={{color: 'rgb(138, 135, 135)'}} />,
+    <MdOutlineLibraryMusic size={32} style={{color: 'rgb(138, 135, 135)'}} />,
+    <MdDeveloperMode size={32} style={{color: 'rgb(138, 135, 135)'}} />, 
+    <BsClipboardData size={32} style={{color: 'rgb(138, 135, 135)'}} /> ,
+    <GiShakingHands size={32} style={{color: 'rgb(138, 135, 135)'}} />,
+    <GiCoffeeCup size={32} style={{color: 'rgb(138, 135, 135)'}} />
     ]
-
 
     const handlerClick = (id) => {
         history.push(`/categoria/${id}`)
     }
+    
     return(
         <>
         <Name>Explora el mercado</Name>
         <LogosGlobal className="divTotal">
             
-            {marketplace && marketplace.map( (logo, index) => (
-                <LogoCard key={index} onClick={() =>handlerClick(logo.id) }>
-                    <Logo src={logo.logo}/>
+            {allCategories && allCategories.map( (logo, index) => (
+                <LogoCard key={index} onClick={() =>handlerClick(logo._id) }>
+                    {allLogos[index]}
                     <NameCard >{logo.name}</NameCard>
                 </LogoCard>
             ))}
