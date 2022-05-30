1ba76a7107f3feeab82009841f92beb08a027be4
MateoM1608 <madime-1997@hotmail.com>
2022-05-30 00:10:30 -0500

    Agrego funcionalidad

--- client/src/components/CardSevices/CardSevices.jsx
+++ client/src/components/CardSevices/CardSevices.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Card,Imagen, Name } from './StyledCardSevices'
 
-const CardSevices = ({name, img}) => {
+const CardSevices = ({name, img, id}) => {
     return (
         <Card>
+            <Link to={`/servicios/${id}`}>
             <Imagen src={img}/>
+            </Link>
             <Name>{name}</Name>
         </Card>
     )
