a93808e44aa9dad151615a38bbb8aa371bf67cc7
MateoM1608 <madime-1997@hotmail.com>
2022-05-30 17:02:43 -0500

    Agrego filtro por categorias

--- client/src/pages/Categories/StyledCategories.js
+++ client/src/pages/Categories/StyledCategories.js
@@ -8,9 +8,9 @@ export const Container = styled.div `
 export const Services = styled.div `
     display: grid;
     flex-direction: row;
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(3, 1fr);
     padding: 25px;
-    padding-left: 50px;
+    padding-left: 25px;
     width: 1340px;
     margin-top: 15px
 `
@@ -35,4 +35,33 @@ export const ContainerBanner = styled.div `
     flex-direction: column;
     justify-content: center;
     align-items: center;
+`
+
+export const ContainerFilter = styled.div `
+    width: 350px;
+    margin-top: 55px;
+    margin-left:50px;
+    border-radius: 25px;
+    border: solid 1px grey;
+`
+
+export const Div = styled.div `
+    display: flex;
+    flex-direction: row
+`
+
+export const Name_filter = styled.h1 `
+    font-size: 20px;
+    margin:20px;
+`
+
+export const Checkbox = styled.div `
+    margin-left: 20px;
+    margin-bottom: 5px;
+`
+
+export const Label = styled.label `
+    margin-left: 10px;
+    
+
 `
