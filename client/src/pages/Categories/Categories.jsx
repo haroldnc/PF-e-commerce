6e92d619e9c05218863f2e8b6a860e947477d031
MateoM1608 <madime-1997@hotmail.com>
2022-05-29 21:45:27 -0500

    Agrego cards de servicios a Pagina categories

--- client/src/pages/Categories/Categories.jsx
+++ client/src/pages/Categories/Categories.jsx
@@ -1,14 +1,40 @@
-import React from 'react'
-import Filters from '../../components/Filters/Filters';
-import Services from "../Services/Services"
+import React, {useEffect} from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { getCategorybyId } from '../../store/actions/index'
+import CardSevices from '../../components/CardSevices/CardSevices';
+import { Container, Services, Name, Banner, ContainerBanner } from './StyledCategories'
 
 const Categories = () => {
-  return (
-    <div>
-      <Filters/>
-      <Services></Services>
-    </div>
-  )
-}
 
-export default Categories
+//  img:"https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto/v1/attachments/generic_asset/asset/a523607ea55cc9551ec824d8f9acc9bb-1626176887661/Search.png"
+  
+
+ const dispatch = useDispatch()
+ const category = useSelector(state => state.category)
+ const {id} = useParams()
+console.log('state',category)
+ console.log(id)
+
+ useEffect(() => {
+   dispatch(getCategorybyId(id))
+ },[dispatch])
+
+  return (
+    <Container>
+      <ContainerBanner>
+        <Banner src={"https://images.unsplash.com/photo-1635830625698-3b9bd74671ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fGRldmVsb3BlcnxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60"} />
+        <Name>{category.name}</Name>
+      </ContainerBanner>
+      <Services>
+        {Object.entries(category).length > 1  ? category.services.map( s => (
+          <div key={s.id}>
+            <CardSevices name={s.name} img={"https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto/v1/attachments/generic_asset/asset/c51dbf5c160dac9bd067442911e65d16-1626181910196/Miscellaneous_2x.png"}/>
+          </div>
+        )): "none"}
+      </Services>
+    </Container>
+  );
+};
+
+export default Categories;
