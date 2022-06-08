84f24e33e85135e7293a5471b19282011ce9db53
Luis de Dios <you@example.com>
2022-06-07 21:41:10 -0500

    workingOn SearchBar

--- client/src/components/Searchbar/Searchbar.jsx
+++ client/src/components/Searchbar/Searchbar.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Container, Input } from './StyledSearchbar'
+import { useDispatch, useSelector } from "react-redux"
+import { getAllPosts } from '../../store/actions'
 
 const Searchbar = () => {
+
+  const dispatch = useDispatch()
+  const allPosts = useSelector((state)=>state.allPost)
+  const [data, setData] = useState("")
+
+  const onSearch = () =>{
+    dispatch(getAllPosts())
+  }
+
+  const handleChange = (e)=>{
+    setData(e.target.value)
+  }
+
   return (
     <Container>
         <Input placeholder="Busca aquí ..."/>
