7b1b8908d558cd6715cb49e5baf52188862e82b0
MateoM1608 <madime-1997@hotmail.com>
2022-06-09 21:49:53 -0500

    Agrego funcionalidad a Mi perfil

--- client/src/components/ProfileBox/ProfileBox.jsx
+++ client/src/components/ProfileBox/ProfileBox.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import { Container } from './StyledProfileBox'
 
-const ProfileBox = ({isOpen,toggleModalSignOut, handleToggle}) => {
+const ProfileBox = ({isOpen,toggleModalSignOut, handleToggle, userInfo}) => {
+
+  const history = useHistory()
+
+  const handleClick = () => {
+    history.push(`/profile/${userInfo.uid}`)
+  }
   return (
     <Container isOpen={isOpen}>
         <ul onClick={handleToggle}>
-                  <li>Perfil</li>
+                  <li onClick={() => handleClick()}>Perfil</li>
                   <li onClick={toggleModalSignOut} >Cerrar sesión</li>
                 </ul>
     </Container>
