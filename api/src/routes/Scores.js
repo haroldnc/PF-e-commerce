4b6f65f154c1633766d5cab98bb656e4d1f2f00d
ludwinner <ludwingbravo4@gmail.com>
2022-06-12 14:37:55 -0300

    modificacion ruta Scores

--- api/src/routes/Scores.js
+++ api/src/routes/Scores.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 const { setScore } = require ('../controllers/Scores/Score.controller');
-const {getScores, getScore} = require ('../controllers/Scores/GetScore.controller');
+const {getScores, getScore,getScoresWorker} = require ('../controllers/Scores/GetScore.controller');
 const { validarADMIN_ROLE } = require('../middlewares/validarAdminRole');
 
 const router = Router()
 
 router.get('/:publicationId', getScores)
+router.get('/:userId', getScoresWorker)
 router.get('/:publicationId/:userId', getScore)
 router.post('/:publicationId',setScore)
 
