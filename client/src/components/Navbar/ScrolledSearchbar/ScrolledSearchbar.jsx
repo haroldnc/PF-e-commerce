19ef6e34ddc9936e80ac50acdb2b18c4702be04a
Luis de Dios <you@example.com>
2022-06-12 20:25:38 -0500

    both searchbar connected

--- client/src/components/Navbar/ScrolledSearchbar/ScrolledSearchbar.jsx
+++ client/src/components/Navbar/ScrolledSearchbar/ScrolledSearchbar.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Container, Input } from './StyledScrolledSearchbar'
+import { getPostByQuery } from '../../../store/actions'
+import { useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom'
+
 
 const ScrolledSearchbar = ({isScrolled}) => {
+
+  const dispatch = useDispatch()
+  const [data, setData] = useState("")
+
+  const onSearch = () =>{
+    if(data.length){
+      dispatch(getPostByQuery(data))
+    }
+    
+  }
+
+  const handleChange = (e)=>{
+    setData(e.target.value)
+  }
+
   return (
     <Container isScrolled={isScrolled}>
-        <Input placeholder="Search here..." />
-        <Button>Search</Button>
+      <Input onChange={handleChange} placeholder="Search here..." />
+      {data?
+        <Link to={"/search"}>
+        <Button onClick={onSearch}>Search</Button>
+        </Link>
+        :
+        <Button onClick={onSearch}>Search</Button>
+      }
     </Container>
   )
 }
