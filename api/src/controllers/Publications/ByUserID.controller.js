e7990dbb4efded51bb44018cf4489a3f7613cbe8
haroldnc <harold.mth95@gmail.com>
2022-06-14 07:21:32 -0500

    corrección de la ruta posts/user/:id

--- api/src/controllers/Publications/ByUserID.controller.js
+++ api/src/controllers/Publications/ByUserID.controller.js
@@ -1,15 +1,24 @@
 const Publications = require('../../models/Publications');
+const DataWorkers = require('../../models/DataWorkers');
 
 const getPostsByUser = async (req, res) => {
    const { id } = req.params;
 
    try {
-      const publicacions = await Publications.find({user: id})
-         .populate('user', { _id:1, firstName:1, lastName:1, image:1 })
-         .populate('service', { _id:1, name:1 })
+      const worker = await DataWorkers.findOne({ userId: id })
          .populate('subscription_type', 'name');
 
-      res.status(200).json(publicacions);
+      const publications = await Publications.find({user: id})
+         .populate('user', { _id:1, firstName:1, lastName:1, image:1 })
+         .populate('service', { _id:1, name:1 });
+
+      res.status(200).json({
+         posts: ...publications,
+         subscription_type: {
+            worker.subscription_type._id,
+            worker.subscription_type.name
+         }
+      });
    } catch (error) {
       res.status(404).json({ error: error.message });
    }
