7e43b624aec9adbd55f05960b047b5e5cc842b1b
ludwinner <ludwingbravo4@gmail.com>
2022-06-15 22:32:34 -0300

    agregar put de hiring

--- api/src/routes/hiring.js
+++ api/src/routes/hiring.js
@@ -5,7 +5,8 @@ const {
     getHiringsByUser, 
     getHiringsByPublication,
     getHiringsByWorker, 
-    deleteHiring 
+    deleteHiring, 
+    putHiring
 } = require('../controllers/hiring.controllers');
 
 const router = Router();
@@ -16,5 +17,6 @@ router.get('/user/:idUser', getHiringsByUser);
 router.get('/publi/:idPublication', getHiringsByPublication);
 router.get('/worker/:idWorker', getHiringsByWorker);
 router.delete('/:id', deleteHiring);
+router.put('/',putHiring);
 
 module.exports = router;
